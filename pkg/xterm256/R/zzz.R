3abf083c5f0e933d87479cc6bfa443a9a8bc50e2
romain (R Enthusiast ) <francoisromain@free.fr>
2009-04-18 13:44:32 +0000

    added details in the way colors can be given
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/highlight@68 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/xterm256/R/zzz.R
+++ pkg/xterm256/R/zzz.R
@@ -4,6 +4,5 @@
 		system.file( "rlogo.txt", package= "xterm256"), 
 		header = F )
 	cat( apply( rlogo.text[,rep(1:ncol(rlogo.text), each = 2)], 1, paste, collapse = ""), sep ="\n" )	
-	options( old.op )
-	
+	options( old.op )	
 }
