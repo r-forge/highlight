bae52cd98e121abce1e213ccacc7ad151a148447
romain (R Enthusiast ) <francoisromain@free.fr>
2009-04-26 09:08:53 +0000

    +documentation print.function, renderer_xterm
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/highlight@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/xterm256/R/highlight_renderer_xterm.R
+++ pkg/xterm256/R/highlight_renderer_xterm.R
@@ -1,24 +1,29 @@
 
-formatter_xterm <- function( tokens, styles, stylesheet = "default", ... ){
-	
-	css <- getStyleFile( stylesheet, "css" )
-	p   <- css.parser( css )
-	dec <- lapply( p, function( declaration ) {
-		out <- list( bg = NA, fg = NA )
-		if( "color" %in% names(declaration) ){
-			out[["fg"]] <- declaration[["color"]]
-		}
-		if( "background" %in% names(declaration) ){
-			out[["bg"]] <- declaration[["bg"]]
-		}
+formatter_xterm <- function(stylesheet){
+	function( tokens, styles, stylesheet = "default", ... ){
+		css <- getStyleFile( stylesheet, "css" )
+		p   <- css.parser( css )
+		dec <- lapply( p, function( declaration ) {
+			out <- list( bg = NA, fg = NA )
+			if( "color" %in% names(declaration) ){
+				out[["fg"]] <- declaration[["color"]]
+			}
+			if( "background" %in% names(declaration) ){
+				out[["bg"]] <- declaration[["bg"]]
+			}
+			out
+		} )
+		dec.fg <- as.character( sapply( dec, "[[", "fg" ) )
+		names( dec.fg ) <- names( dec) 
+		
+		dec.bg <- as.character( sapply( dec, "[[", "bg" ) )
+		names( dec.bg ) <- names( dec) 
+		
+		out <- style( tokens, 
+			bg = if( !all(is.na(dec.bg) ) ) dec.bg[styles], 
+			fg = if( !all(is.na(dec.fg) ) ) dec.fg[styles] )
 		out
-	} )
-	dec.fg <- as.character( sapply( dec, "[[", "fg" ) ); names( dec.fg ) <- names( dec) 
-	dec.bg <- as.character( sapply( dec, "[[", "bg" ) ); names( dec.bg ) <- names( dec) 
-	out <- style( tokens, 
-		bg = if( !all(is.na(dec.bg) ) ) dec.bg[styles], 
-		fg = if( !all(is.na(dec.fg) ) ) dec.fg[styles] )
-	out
+	}
 }
 
 translator_xterm <- function( x ){
@@ -42,9 +47,11 @@ footer_xterm <- function( document ){
 }
 
 renderer_xterm <- function(
-	translator = translator_xterm, formatter = formatter_xterm, 
+	translator = translator_xterm, 
+	formatter = formatter_xterm( stylesheet = stylesheet ), 
 	space = space_xterm, newline = newline_xterm, 
-	header = header_xterm, footer = footer_xterm ,  
+	header = header_xterm, footer = footer_xterm, 
+	stylesheet = "default", 
 	... ){
 	
 	renderer( translator = translator, formatter = formatter, 
