5665198b177284227afd9037df7a1a994c18d879
romain (R Enthusiast ) <francoisromain@free.fr>
2009-06-16 10:22:04 +0000

    added prompt and continue to the highlighted results (based on a bit of a hack, ..)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/highlight@156 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/xterm256/R/zzz.R
+++ pkg/xterm256/R/zzz.R
@@ -1,8 +1,8 @@
 .onAttach <- function( libname, pkgname ){
-	old.op <- options( width = 100 )
-	rlogo.text <- read.table( 
-		system.file( "rlogo.txt", package= "xterm256"), 
-		header = F )
-	cat( apply( rlogo.text[,rep(1:ncol(rlogo.text), each = 2)], 1, paste, collapse = ""), sep ="\n" )	
-	options( old.op )	
+	# old.op <- options( width = 100 )
+	# rlogo.text <- read.table( 
+	# 	system.file( "rlogo.txt", package= "xterm256"), 
+	# 	header = F )
+	# cat( apply( rlogo.text[,rep(1:ncol(rlogo.text), each = 2)], 1, paste, collapse = ""), sep ="\n" )	
+	# options( old.op )	
 }
