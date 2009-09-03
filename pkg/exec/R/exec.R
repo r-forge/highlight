a8fca9d70a9447b11c42fe93659064d51daeef90
romain (R Enthusiast ) <francoisromain@free.fr>
2009-09-03 11:44:49 +0000

    added rscript function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/highlight@199 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/exec/R/exec.R
@@ -0,0 +1,15 @@
+rscript <- function( package, script, arguments = commandArgs(TRUE), run = TRUE ){
+	
+	Rscript <- file.path( R.home(), "bin", "Rscript" )
+	script  <- system.file( 'exec', script, package = package )
+	
+	arguments <- if( length( arguments ) == 0 ) "" else paste( arguments, collapse = " " )
+	cmd <- sprintf( '%s "%s" %s', Rscript, script, arguments )
+	
+	if( run ){
+		system( cmd )
+	} else {
+		cmd
+	}
+}
+
