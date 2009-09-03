9c45bf9aacd8a3ba5c119a95c99c90500f14db91
romain (R Enthusiast ) <francoisromain@free.fr>
2009-09-03 12:59:24 +0000

    added a dir argument to rscript
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/highlight@201 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/exec/R/exec.R
+++ pkg/exec/R/exec.R
@@ -1,7 +1,10 @@
-rscript <- function( package, script, arguments = commandArgs(TRUE), run = TRUE ){
+rscript <- function( package, script, arguments = commandArgs(TRUE), run = TRUE, dir = "exec" ){
 	
 	Rscript <- file.path( R.home(), "bin", "Rscript" )
-	script  <- system.file( 'exec', script, package = package )
+	script  <- system.file( dir, script, package = package )
+	if( !file.exists( script ) ){
+		stop( sprintf( "script '%s' does not exist", script) )
+	}
 	
 	arguments <- if( length( arguments ) == 0 ) "" else paste( arguments, collapse = " " )
 	cmd <- sprintf( '"%s" "%s" %s', Rscript, script, arguments )
