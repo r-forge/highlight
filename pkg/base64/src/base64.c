8ea2d558de7ec1a1d0915d4185b8b1a3ca9e29c6
leisch (Friedrich Leisch) <friedrich.leisch@r-project.org>
2012-08-16 13:59:49 +0000

    take over from Romain: clarify copyright, no C++ comments
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/highlight@340 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/base64/src/base64.c
+++ pkg/base64/src/base64.c
@@ -1,3 +1,20 @@
+/* Copyright (c) 2001 Bob Trower, Trantor Standard Systems Inc.
+ * Copyright (c) 2010-2011 Romain Francois
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, a copy is available at
+ *  http://www.r-project.org/Licenses/
+ */
 
 #include "base64.h"
 
@@ -160,12 +177,12 @@ int b64( int opt, char *infilename, char *outfilename, int linesize )
     }
     else {
         FILE *outfile;
-        // if( !outfilename ) {
-        //     outfile = stdout;
-        // }
-        // else {
+        /* if( !outfilename ) {
+         *     outfile = stdout;
+         * }
+         * else { */
             outfile = fopen( outfilename, "wb" );
-        // }
+	    /* } */
         if( !outfile ) {
             perror( outfilename );
         }
@@ -182,12 +199,12 @@ int b64( int opt, char *infilename, char *outfilename, int linesize )
             else {
                  retcode = 0;
             }
-            // if( outfile != stdout ) {
+            /* if( outfile != stdout ) { */
                 if( fclose( outfile ) != 0 ) {
                     perror( b64_message( B64_ERROR_OUT_CLOSE ) );
                     retcode = B64_FILE_IO_ERROR;
                 }
-            // }
+  	    /* } */
         }
         if( infile != stdin ) {
             fclose( infile );
